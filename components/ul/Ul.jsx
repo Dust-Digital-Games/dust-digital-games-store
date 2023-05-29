472c9bfbdab0f00c881dfca19c464755f52454ae
Julian <julianlopez4301@gmail.com>
2023-05-29 20:22:38 -0300

    componente ul

---
+++ components/ul/Ul.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Ul({ container }) {
+  return (
+    <ul className={container}>
+      <li>Games</li>
+      <li>New!</li>
+      <li>Categories</li>
+      <li>About</li>
+      <li>luna</li>
+    </ul>
+  );
+}
