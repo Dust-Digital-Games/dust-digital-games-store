bba0ffc83445b1ac70ec06538e80f3147d313a73
Julian <julianlopez4301@gmail.com>
2023-05-31 21:38:36 -0300

    Agregué el titulo y la card comentada

--- components/Home/HomeCard/HomeCard.tsx
+++ components/Home/HomeCard/HomeCard.tsx
@@ -1,5 +1,11 @@
 import React from "react";
+import Card from "./Card/Card";
 
 export default function HomeCard() {
-  return <div>HomeCard</div>;
+  return (
+    <section>
+      <h2>¡Check what's new!</h2>
+      {/* <Card /> */}
+    </section>
+  );
 }
