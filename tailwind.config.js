2be6e26cbd286c2ae9ae1db75dc409529d655cf9
Julian <julianlopez4301@gmail.com>
2023-06-03 15:59:18 -0300

    agregue colores

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,8 @@ module.exports = {
         primaryLetter: "#2C41FF",
         border: "1px solid red",
         neutral: "#1A1A1B",
+        textHomecardRD: "rgb(136,136,136)",
+        bgHomecardPlatforms: "#CFD4FF",
       },
     },
   },
