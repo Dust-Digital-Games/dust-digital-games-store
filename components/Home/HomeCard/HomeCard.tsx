9f02803206c5c4d1909005606e2ca5925aaad966
Julian <julianlopez4301@gmail.com>
2023-06-04 13:26:32 -0300

    Probando como se ve, faltan detalles

--- components/Home/HomeCard/HomeCard.tsx
+++ components/Home/HomeCard/HomeCard.tsx
@@ -1,11 +1,20 @@
 import React from "react";
 import Card from "./Card/Card";
-
+import Stray from "../../../assets/stray.jpg";
 export default function HomeCard() {
   return (
-    <section>
+    <section className="w-full flex flex-col justify-center items-center gap-5 mt-5 text-2xl text-primaryLetter font-semibold">
       <h2>¡Check what's new!</h2>
-      {/* <Card /> */}
+      <Card
+        image={Stray}
+        targetDiscount="-20%"
+        platforms="Indie"
+        releaseData="15 Jun 2023"
+        name="Stray"
+        rating={4.9}
+        info="asdasdasdasdasdasdsadasdasdsds"
+        discount={false}
+      />
     </section>
   );
 }
