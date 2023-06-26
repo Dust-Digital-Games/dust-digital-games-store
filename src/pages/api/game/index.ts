05dbb3a55bd410364b0d2e8a4ed352c078ddee95
lucaspiritogit <lucasignaciopirito@gmail.com>
2023-06-26 02:09:56 -0300

    creating endpoint to return all games without pagination WIP

--- src/pages/api/game/index.ts
+++ src/pages/api/game/index.ts
@@ -1,4 +1,4 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
+import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../../lib/prisma';
 
 type GameCategories = [
@@ -7,31 +7,40 @@ type GameCategories = [
   }
 ];
 
-export default async function uploadGame(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Bad request' });
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (req.method === 'GET') {
+    try {
+      const games = await prisma.game.findMany();
+      return res.status(200).json(games);
+    } catch (error) {
+      return res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
+  
+  if (req.method === 'POST') {
+    const gameData = req.body;
+    const gameCategories: GameCategories = gameData.categories.connect;
 
-  const gameData = req.body;
-  const gameCategories: GameCategories = gameData.categories.connect;
-
-  try {
-    const game = await prisma.game.create({
-      data: {
-        contractAddress: gameData.contractAddress,
-        game_name: gameData.gameName,
-        description: gameData.description,
-        image: gameData.image,
-        download_link: gameData.downloadLink,
-        categories: {
-          connect: gameCategories,
+    try {
+      const game = await prisma.game.create({
+        data: {
+          contractAddress: gameData.contractAddress,
+          game_name: gameData.gameName,
+          description: gameData.description,
+          image: gameData.image,
+          download_link: gameData.downloadLink,
+          categories: {
+            connect: gameCategories,
+          },
         },
-      },
-      include: { categories: true },
-    });
-    return res.status(200).json([{ message: 'Game uploaded' }, { game: game }]);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json({ message: 'Internal server error' });
+        include: { categories: true },
+      });
+      return res.status(200).json([{ message: 'Game uploaded' }, { game: game }]);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: 'Internal server error' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
   }
 }
